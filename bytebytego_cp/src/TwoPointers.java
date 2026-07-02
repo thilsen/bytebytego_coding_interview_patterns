cf9056d5b5e33e738f3cf5cbf24f97c2b3ede529
Senthil Kumar N <>
2026-07-02 18:56:26 +0530

    triplet finding

--- bytebytego_cp/src/TwoPointers.java
+++ bytebytego_cp/src/TwoPointers.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class TwoPointers {
     public static void main(String[] args) {
@@ -12,6 +13,15 @@ public class TwoPointers {
         int target = 6;
         ArrayList<Integer> ans = tp.pair_sum_sorted(nums, target);
         System.out.println(ans);
+
+        ArrayList<Integer> nums1 = new ArrayList<>();
+        nums1.add(0);
+        nums1.add(-1);
+        nums1.add(2);
+        nums1.add(-3);
+        nums1.add(1);
+        ArrayList<ArrayList<Integer>> triplets = tp.triplet_sum(nums1);
+        System.out.println(triplets);
     }
 
     /*
@@ -27,6 +37,12 @@ public class TwoPointers {
         Input: nums = [1, 1, 1], target = 2
         Output: [0, 1]
         Explanation: other valid outputs could be [1, 0], [0, 2], [2, 0], [1, 2] or [2, 1].
+     */
+    /*
+    Complexity Analysis
+        Time complexity: The time complexity of pair_sum_sorted is O(n) because we perform approximately
+        n iterations using the two-pointer technique in the worst case.
+        Space complexity: We only allocated a constant number of variables, so the space complexity is O(1).
      */
     public ArrayList<Integer> pair_sum_sorted(ArrayList<Integer> nums, int target) {
         ArrayList<Integer> ans = new ArrayList<>();
@@ -45,5 +61,62 @@ public class TwoPointers {
             }
         }
         return ans;
+    }
+
+    /*
+    Triplet Sum
+        Given an array of integers, return all triplets [a, b, c] such that a + b + c = 0 . The solution must not contain duplicate triplets (e.g., [1, 2, 3] and [2, 3, 1] are considered duplicates). If no such triplets are found, return an empty array.
+
+        Each triplet can be arranged in any order, and the output can be returned in any order.
+
+        Example:
+        Input: nums = [0, -1, 2, -3, 1]
+        Output: [[-3, 1, 2], [-1, 0, 1]]
+     */
+    public ArrayList<ArrayList<Integer>> triplet_sum(ArrayList<Integer> nums) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        Collections.sort(nums);
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if(nums.get(i) > 0) {
+                break;
+            }
+            //Avoid duplicates
+            if(i > 0 && nums.get(i) == nums.get(i - 1)) {
+                continue;
+            }
+            int target = -nums.get(i);
+            ArrayList<ArrayList<Integer>> pairs = triplet_sum_helper(nums, target, i +  1);
+            for(ArrayList<Integer> pair : pairs) {
+                ArrayList<Integer> triplet = new ArrayList<>();
+                triplet.add(nums.get(i));
+                triplet.addAll(pair);
+                result.add(triplet);
+            }
+        }
+        return result;
+    }
+    private ArrayList<ArrayList<Integer>> triplet_sum_helper(ArrayList<Integer> nums, int target, int start) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        int left = start;
+        int right = nums.size() - 1;
+        while (left < right) {
+            int sum = nums.get(left) + nums.get(right);
+            if(sum == target) {
+                ArrayList<Integer> triplet = new ArrayList<>();
+                triplet.add(nums.get(left));
+                triplet.add(nums.get(right));
+                result.add(triplet);
+                left++;
+                while(left < right && nums.get(left) == nums.get(left - 1)) {
+                    left++;
+                }
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return result;
     }
 }
